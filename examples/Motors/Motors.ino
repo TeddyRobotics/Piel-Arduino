ede1fd9a39b8b25c5890eb7ff372ca4afa90a0f6
wberreta <walterberreta@gmail.com>
2020-10-26 13:28:24 -0400

    Motors move example

---
+++ examples/Motors/Motors.ino
@@ -0,0 +1,19 @@
+#include <Piel.h>
+
+Piel pielRobot;
+
+void setup() {
+  pielRobot.Initialize();
+}
+
+void loop() {
+  // 0-100%
+  int speed = 50;
+  pielRobot.move_forward(speed);
+  //pielRobot.move_back(speed);
+  //pielRobot.move_left(speed);
+  //pielRobot.move_right(speed);
+  delay(5);
+  pielRobot.stop();
+  delay(1);
+}
